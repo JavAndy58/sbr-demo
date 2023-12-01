e747112eca523139038137beaa5faae8d4f90f16
JavAndy58 <digital.designs.russia@gmail.com>
2023-12-01 17:51:04 +0300

    create db mysql

--- src/main/java/com/javandy/sbrdemo/controller/StudentController.java
+++ src/main/java/com/javandy/sbrdemo/controller/StudentController.java
@@ -24,4 +24,19 @@ public class StudentController {
     public Student addStudent(@RequestBody Student student) {
         return studentService.addStudent(student);
     }
+
+    @PutMapping("/update/{id}")
+    public Student updateStudent(@RequestBody Student student, @PathVariable Long id) {
+        return studentService.updateStudent(student, id);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteStudent(@PathVariable Long id) {
+        studentService.deleteStudent(id);
+    }
+
+    @GetMapping("/student/{id}")
+    public Student getStudentById(@PathVariable Long id) {
+        return studentService.getStudentById(id);
+    }
 }
